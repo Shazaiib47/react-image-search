734ef07cad2de214e508514dda086c2dc11351b1
Shazaib <shazaib47@hotmail.com>
2024-04-19 14:50:17 +0100

    added onsubmit handler to form and created handlesearch method

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Form } from 'react-bootstrap';
 import './index.css';
 
 const App = () => {
+  const searchInput = useRef(null);
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+    console.log('submitted');
+  };
+
   return (
     <div className='container'>
       <h1 className='title'>Image Search</h1>
       <div className='search-section'>
-        <Form>
+        <Form onSubmit={handleSearch}>
           <Form.Control
             type='search'
             placeholder='Type something to search...'
             className='search-input'
+            ref={searchInput}
           />
         </Form>
       </div>
