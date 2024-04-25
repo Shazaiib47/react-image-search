26b95f29151654db2ce0202d70528f18995e2805
Shazaib <shazaib47@hotmail.com>
2024-04-25 11:06:47 +0100

    refactored code and added logic for loading indication for images

--- src/App.jsx
+++ src/App.jsx
@@ -11,51 +11,52 @@ const App = () => {
   const [images, setImages] = useState([]);
   const [totalPages, setTotalPages] = useState(0);
   const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
 
-  
-  const fetchImages = useCallback(async () => {
+  const fetchImages = useCallback(async (pageNumber) => {
     try {
       if (searchInput.current.value) {
+        setLoading(true);
         const { data } = await axios.get(
           `${API_URL}?query=${
             searchInput.current.value
-          }&page=${page}&per_page=${IMAGES_PER_PAGE}&client_id=${
+          }&page=${pageNumber}&per_page=${IMAGES_PER_PAGE}&client_id=${
             import.meta.env.VITE_API_KEY
           }`
         );
         console.log('data', data);
         setImages(data.results);
         setTotalPages(data.total_pages);
+        setLoading(false);
       }
     } catch (error) {
       console.log(error);
+      setLoading(false);
     }
-  }, [page]);
-  
+  }, []);
 
   useEffect(() => {
-    fetchImages();
+    fetchImages(page);
   }, [fetchImages, page]);
 
   const resetSearch = () => {
     setPage(1);
     fetchImages(1);
-  }
+  };
 
   const handleSearch = (event) => {
     event.preventDefault();
     console.log(searchInput.current.value);
-    fetchImages();
+    fetchImages(page);
     resetSearch();
   };
 
   const handleSelection = (selection) => {
     searchInput.current.value = selection;
-    fetchImages();
+    fetchImages(page);
     resetSearch();
     console.log('page', page);
   };
-
 
   return (
     <div className='container'>
@@ -76,24 +77,30 @@ const App = () => {
         <div onClick={() => handleSelection('cats')}>Cats</div>
         <div onClick={() => handleSelection('shoes')}>Shoes</div>
       </div>
-      <div className='images'>
-        {images.map((image) => (
-            <img
-              key={image.id}
-              src={image.urls.small}
-              alt={image.alt_description}
-              className='image'
-            />
-        ))}
-    </div>
-    <div className='buttons'>
-    {page > 1 && (
-    <Button onClick={() => setPage(page - 1)}>Previous</Button>
-  )}
-  {page < totalPages && (
-    <Button onClick={() => setPage(page + 1)}>Next</Button>
-  )}
-    </div>
+      {loading ? (
+        <p className='loading'>Loading...</p>
+      ) : (
+        <>
+          <div className='images'>
+            {images.map((image) => (
+              <img
+                key={image.id}
+                src={image.urls.small}
+                alt={image.alt_description}
+                className='image'
+              />
+            ))}
+          </div>
+          <div className='buttons'>
+            {page > 1 && (
+              <Button onClick={() => setPage(page - 1)}>Previous</Button>
+            )}
+            {page < totalPages && (
+              <Button onClick={() => setPage(page + 1)}>Next</Button>
+            )}
+          </div>
+        </>
+      )}
     </div>
   );
 };
